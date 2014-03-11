8d0b50e239cf80b91e162c74b71a82191b7bac6d
t04st3r <raffaele.tosti@studenti.unipg.it>
2014-03-11 18:11:33 +0100

    Styling Splash Activity

--- CollabRoute/src/main/java/it/digisin/collabroute/SplashActivity.java
+++ CollabRoute/src/main/java/it/digisin/collabroute/SplashActivity.java
@@ -8,12 +8,19 @@ import android.os.Message;
 import android.os.SystemClock;
 import java.lang.ref.WeakReference;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
 
 /**
  * Created by raffaele on 10/03/14.
  */
 public class SplashActivity extends Activity {
+
+    private static final String IS_DONE_KEY = "it.digisin.collabroute.key.IS_DONE_KEY";
+
+    private static final String START_TIME_KEY = "it.digisin.collabroute.key.START_TIME_KEY";
 
     private static final String TAG_LOG = SplashActivity.class.getName();
 
@@ -23,7 +30,7 @@ public class SplashActivity extends Activity {
 
     private static final int GO_AHEAD_WHAT = 1;
 
-    private long mStartTime;
+    private long mStartTime = -1L;
 
     private boolean mIsDone;
 
@@ -59,15 +66,50 @@ public class SplashActivity extends Activity {
 
     private UiHandler mHandler;
 
+
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        if(savedInstanceState != null){
+            this.mStartTime = savedInstanceState.getLong(START_TIME_KEY);
+        }
         mHandler = new UiHandler(this);
+        final ImageView logoImageView = (ImageView) findViewById(R.id.splash_imageview);
+        logoImageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                Log.d(TAG_LOG, "ImageView touched!!");
+                long elapsedTime = SystemClock.uptimeMillis() - mStartTime;
+                if (elapsedTime >= MIN_WAIT_INTERVAL && !mIsDone) {
+                    mIsDone = true;
+                    goAhead();
+                } else {
+                    Log.d(TAG_LOG, "Too much early!");
+                }
+                return false;
+            }
+        });
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(IS_DONE_KEY, mIsDone);
+        outState.putLong(START_TIME_KEY, mStartTime);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstance){
+        super.onRestoreInstanceState(savedInstance);
+        this.mIsDone = savedInstance.getBoolean(IS_DONE_KEY);
+    }
+
     @Override
     protected void onStart(){
         super.onStart();
-        mStartTime = SystemClock.uptimeMillis();
+        if(mStartTime == -1L) {
+            mStartTime = SystemClock.uptimeMillis();
+        }
         final Message goAheadMessage = mHandler.obtainMessage(GO_AHEAD_WHAT);
         mHandler.sendMessageAtTime(goAheadMessage, mStartTime + MAX_WAIT_INTERVAL);
     }
