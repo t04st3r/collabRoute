57be288602ca7e2358c234e9a6572c0ac40c467f
t04st3r <raffaele.tosti@studenti.unipg.it>
2014-03-15 19:09:43 +0100

    Revert "implemented LoginHandler class"
    This reverts commit f16a9753c0bb1bbece7b26f659f4281dc34f35c7.

--- CollabRoute/src/main/java/it/digisin/collabroute/UserLoginHandler.java
+++ CollabRoute/src/main/java/it/digisin/collabroute/UserLoginHandler.java
@@ -1,12 +1,5 @@
 package it.digisin.collabroute;
 
-import android.app.Activity;
-import android.util.Log;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import javax.net.ssl.HttpsURLConnection;
-
 /**
  * Created by raffaele on 12/03/14.
  */
@@ -22,18 +15,6 @@ public class UserLoginHandler {
     }
 
     public boolean logIn(){
-      try {
-            URL url = new URL("https://"+serverUrl+":"+serverPort+"/auth/"+user.getEMail()+"/"+user.getPassword());
-            HttpsURLConnection con = (HttpsURLConnection)url.openConnection();
-            String response = con.getResponseMessage();
-            Log.e(LoginActivity.TAG_LOG , response);
-      } catch (MalformedURLException e) {
-            Log.e(LoginActivity.TAG_LOG, e.getMessage());
-      } catch (IOException e) {
-          Log.e(LoginActivity.TAG_LOG, e.getMessage());
-      } catch (Exception e){
-          e.printStackTrace();
-      }
-      return true;
+        return true;
     }
 }
